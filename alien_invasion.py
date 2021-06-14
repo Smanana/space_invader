b1d26349c8c575d9907e4ec1214b4268e3d78f10
smanana <smanana@student.wethinkcode.co.za>
2021-06-14 16:24:11 +0200

    Added new files, inseted and image,can move space ship from left to right

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,16 +1,23 @@
-import sys
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    alien_invasion_settings = Settings()
+    screen = pygame.display.set_mode(
+        (alien_invasion_settings.screen_width,alien_invasion_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    #Make a ship
+    ship = Ship(alien_invasion_settings, screen)
+    
     # Start the main loop for the game.
     while True:
-    # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-    # Make the most recently drawn screen visible.
-    pygame.display.flip()
+        # Watch for keyboard and mouse events.
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(alien_invasion_settings,screen,ship)
+
 run_game()
---
+++ ship.py
@@ -0,0 +1,42 @@
+import pygame
+
+class Ship():
+
+    def __init__(self,alien_invasion_settings,screen):
+        """Initialize the ship and set its starting position"""
+        self.screen = screen
+        self.alien_invasion_settings = alien_invasion_settings
+
+        #Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.transform.scale(self.image, (50, 70))
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        #Start each new ship at the bottom center of the screen.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        #Store a decimal value for the ship's center
+        self.center = float(self.rect.centerx)
+
+        #Movement Flag
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+
+    def update(self):
+        """Update the ship's position baed on the movement flag"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.alien_invasion_settings.ship_speed_factor
+
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.alien_invasion_settings.ship_speed_factor
+
+        if self.moving_up and self.rect.up < self.screen_rect.up:
+            self.center += self.alien_invasion_settings.ship_speed_factor
+        #Update rect object from self.center
+        self.rect.centerx =self.center
+    def blitme(self):
+        """Draw the ship at its current location"""
+        self.screen.blit(self.image,self.rect)
